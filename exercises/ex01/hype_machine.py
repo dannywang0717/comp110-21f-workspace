e4e90d704d4641480d2e49eacd051c00cc6ad593
Danny Wang <dwang1@unc.edu>
2021-08-29 23:35:10 -0400

    Progress on Exercise 1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,9 @@
 # TODO: Write docstring here
-
+"""A program to boost my spirit."""
 # TODO: Initialize __author__ variable here
-
+__author__ = "730490041"
 # TODO: Implement your program logic here
+name: str = input("What is your name? ")
+print((name) + " is the best")
+print("That is correct " + (name) + ". You are Awesome!")
+print("Have a good day " + (name))
---
+++ exercises/ex01/numeric_operators.py
@@ -0,0 +1,8 @@
+"""Shows how the four numeric operators work in python."""
+__author__ = "730490041"
+left: int = (int(input("Left-hand side: ")))
+right: int = (int(input("Right-hand side: ")))
+print(str(left) + " ** " + str(right) + " is " + str(left ** right))
+print(str(left) + " ** " + str(right) + " is " + str(left / right))
+print(str(left) + " ** " + str(right) + " is " + str(left // right))
+print(str(left) + " ** " + str(right) + " is " + str(left % right))
