79c9d5ed12e6f6301535bf773228ddd4d5853961
Danny Wang <dwang1@unc.edu>
2021-09-12 18:57:41 -0400

    ex03 finished

--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,11 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730490041"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+num: int = int(input("Depth: "))
+i: int = 1
+while i <= num:
+    print(TREE * (i))
+    i = i + 1
--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -1,6 +1,15 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730490041"
 
 
 # Begin your solution here...
+num: int = int(input("Enter an int: "))
+if num % 2 == 0 and num % 7 == 0:
+    print("TAR HEELS")
+elif num % 2 == 0:
+    print("TAR")
+elif num % 7 == 0:
+    print("HEELS")
+elif num % 2 != 0 and num % 7 != 0:
+    print("CAROLINA")
