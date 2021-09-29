442bc74679e0b4f760c4cf5b5f7618c4f770c2e2
Danny Wang <dwang1@unc.edu>
2021-09-28 22:08:57 -0400

    list exercise updated

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -5,6 +5,7 @@ __author__ = "730490041"
 
 # TODO: Implement your functions here.
 def all(a: list[int], b: int) -> bool:
+    """Test if all values in a list are the same. """
     i: int = 0
     count: int = 0
     if len(a) > 0:
@@ -20,6 +21,7 @@ def all(a: list[int], b: int) -> bool:
 
 
 def is_equal(a: list[int], b: list[int]) -> bool:
+    """Test if two lists are equal. """
     if len(a) == len(b):
         i: int = 0
         num: int = 0
@@ -35,6 +37,7 @@ def is_equal(a: list[int], b: list[int]) -> bool:
 
 
 def max(input: list[int]) -> int:
+    """Gets the max value of a list of int. """
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     else:
