0a4e4275cf279328c2e3d3b00a691feeed9fde88
Danny Wang <dwang1@unc.edu>
2021-10-05 20:34:08 -0400

    ex05 completet

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,39 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730490041"
 
 # Define your functions below
+
+
+def only_evens(a: list[int]) -> list[int]:
+    """Returns all even numbers in a give list."""
+    evens = []
+    for i in a:
+        if i % 2 == 0:
+            evens.append(i)
+
+    return evens
+
+
+def sub(a: list[int], b: int, c: int) -> list[int]:
+    """Returns a sublist given a start and end index."""
+    if len(a) == 0:
+        return []
+    elif b < 0:
+        return a[0:c]
+    elif c > len(a):
+        return a[b:len(a)]
+    return a[b:c]
+
+
+def concat(a: list[int], b: list[int]) -> list[int]:
+    """Concats two list together."""
+    new = []
+    new = a + b
+    return new
+
+
+num = [1, 2, 3, 4, 5, 6]
+num2 = [1, 2, 3, 4, 5, 6, 7]
+
+print(concat(num, num2))
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,69 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730490041"
+
+
+def test_only_evens() -> None:
+    """Edge Case Test for the only_evens function."""
+    xs: list[int] = [2]
+    assert only_evens(xs)
+
+
+def test_only_evens_1() -> None:
+    """First Use Case Test for the only_evens function."""
+    xs: list[int] = [1, 2, 3, 4, 5]
+    assert only_evens(xs)
+
+
+def test_only_evens_2() -> None:
+    """Second Use Case Test for the only_evens function."""
+    xs: list[int] = [2, 2, 4, 8, 9]
+    assert only_evens(xs)
+
+
+def test_sub() -> None:
+    """Edge Case Test for the sub function."""
+    xs: list[int] = []
+    a: int = 1
+    b: int = 5
+    assert sub(xs, a, b)
+
+
+def test_sub_1() -> None:
+    """First Use Case Test for the sub function."""
+    xs: list[int] = [1, 2, 3, 4, 5]
+    a: int = 1
+    b: int = 3
+    assert sub(xs, a, b)
+
+
+def test_sub_2() -> None:
+    """Second Use Case Test for the sub function."""
+    xs: list[int] = [2, 3, 4, 5]
+    a: int = 1
+    b: int = 4
+    assert sub(xs, a, b)
+
+
+def test_concat() -> None:
+    """Edge Case Test for the concat function."""
+    xs: list[int] = [1]
+    xt: list[int] = [1]
+    assert concat(xs, xt)
+
+
+def test_concat_1() -> None:
+    """First Use Case Test for the concat function."""
+    xs: list[int] = [1, 2, 3, 4]
+    xt: list[int] = [1, 2, 3]
+    assert concat(xs, xt)
+
+
+def test_concat_2() -> None:
+    """Second Use Case Test for the concat function."""
+    xs: list[int] = [1, 4, 4]
+    xt: list[int] = [1, 2, 3]
+    assert concat(xs, xt)
