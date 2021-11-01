1743e32c6c3f9124307e8a41d46a6fb1a6dec468
Danny Wang <dwang1@unc.edu>
2021-11-01 19:35:42 -0400

    dictionary

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,39 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730490041"
 
 # Define your functions below
+
+
+def invert(a: dict[str, str]) -> dict[str, str]:
+    """Inverts the key and the value of a dictionary."""
+    result: dict[str, str] = {}
+    result = {x: y for y, x in a.items()}
+    return result
+
+
+def favorite_color(b: dict[str, str]) -> str:
+    """Finds the favorite color of a dictionary."""
+    x = b.values()
+    duplicate = []
+    unique = []
+    for i in x:
+        if i in unique:
+            duplicate.append(i)
+        else:
+            unique.append(i)
+    if len(duplicate) > 0:
+        return str(duplicate[0])
+    else:
+        return str(unique[0])
+    
+
+def count(c: list[str]) -> dict[str, int]:
+    """Creats a dictionary given a list of string and the value is the count it repeats."""
+    empty = {}
+    for i in c:
+        if i in empty.keys():
+            empty[i] += 1
+        else:
+            empty[i] = 1
+    return empty     
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,60 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730490041"
+
+
+def test_invert() -> None:
+    """Edge Case Test for the invert function."""
+    xs: dict[str, str] = {"a": "z"}
+    assert invert(xs)
+
+
+def test_invert_1() -> None:
+    """First Use Case Test for the invert function."""
+    xs: dict[str, str] = {"a": "z", "apple": "zebra"}
+    assert invert(xs)
+
+
+def test_invert_2() -> None:
+    """Second Use Case Test for the invert function."""
+    xs: dict[str, str] = {"a": "z", "cat": "dog", "one": "two"}
+    assert invert(xs)
+
+
+def test_favorite_color() -> None:
+    """Edge Case Test for the favorite colors function."""
+    xs: dict[str, str] = {"a": "red"}
+    assert favorite_color(xs)
+
+
+def test_favorite_color_1() -> None:
+    """First Use Case Test for the favorite colors function."""
+    xs: dict[str, str] = {"a": "red", "b": "red", "c": "blue", "d": "black"}
+    assert favorite_color(xs)
+
+
+def test_favorite_color_2() -> None:
+    """Second Use Case Test for the favorite colors function."""
+    xs: dict[str, str] = {"a": "red", "b": "red", "c": "red", "d": "black"}
+    assert favorite_color(xs)
+
+
+def test_count() -> None:
+    """Edge Case Test for the count function."""
+    xs: list = ["a"]
+    assert count(xs)
+
+
+def test_count_1() -> None:
+    """First Use Case Test for the count function."""
+    xs: list = ["a", "a", "b"]
+    assert count(xs)
+
+
+def test_count_2() -> None:
+    """Second Use Case Test for the count function."""
+    xs: list = ["a", "a", "a", "c", "c", "b"]
+    assert count(xs)
